3f9e243929a4d8a75fb9d84741eb561dba30ec93
itvn9online <itvn9online@gmail.com>
2021-12-28 00:40:42 +0700

    Update admin_teamplate.php

--- app/Views/admin/admin_teamplate.php
+++ app/Views/admin/admin_teamplate.php
@@ -33,7 +33,10 @@ if ( file_exists( THEMEPATH . 'custom/admin/autoload.php' ) ) {
 <head>
 <title>Quản trị</title>
 <meta charset="UTF-8"/>
-<meta http-equiv="Cache-control" content="public">
+<!-- <meta http-equiv="Cache-control" content="public"> -->
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
 <meta name="robots" content="noindex, nofollow" />
 <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"/> -->
 <base href="<?php echo DYNAMIC_BASE_URL; ?>" />
