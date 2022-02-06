84139a022c79cc0def6ff52f43163fbaf8fe1eb8
itvn9online <itvn9online@gmail.com>
2022-02-07 04:52:35 +0700

    Update update_code.js

--- public/admin/js/update_code.js
+++ public/admin/js/update_code.js
@@ -1,11 +1,15 @@
 //
 function done_submit_update_code() {
+    $('body').css({
+        'opacity': 1
+    });
     document.frm_global_upload.reset();
-
-    //
     WGR_alert('Update và giải nén code thành công');
 }
 
 function auto_submit_update_code() {
+    $('body').css({
+        'opacity': 0.1
+    });
     document.frm_global_upload.submit();
 }
