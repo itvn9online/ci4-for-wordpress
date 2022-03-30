5a86d72bcdaeb4cfe65286bf63f51f9f151d075d
itvn9online <itvn9online@gmail.com>
2022-03-31 05:40:40 +0700

    Update Logger.php

--- app/Config/Logger.php
+++ app/Config/Logger.php
@@ -37,7 +37,7 @@ class Logger extends BaseConfig
      *
      * @var array|int
      */
-    public $threshold = 4;
+    public $threshold = 9;
 
     /**
      * --------------------------------------------------------------------------
