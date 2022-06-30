87a0dda74dba3b0bb5cf3a0551113e6d22a5b908
itvn9online <itvn9online@gmail.com>
2022-06-30 07:14:04 +0700

    Update Constants.php

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -13,7 +13,7 @@
  | NOTE: changing this will require manually modifying the
  | existing namespaces of App\* namespaced-classes.
  */
-defined( 'APP_NAMESPACE' ) || define( 'APP_NAMESPACE', 'App' );
+defined('APP_NAMESPACE') || define('APP_NAMESPACE', 'App');
 
 /*
  | --------------------------------------------------------------------------
@@ -23,7 +23,7 @@ defined( 'APP_NAMESPACE' ) || define( 'APP_NAMESPACE', 'App' );
  | The path that Composer's autoload file is expected to live. By default,
  | the vendor folder is in the Root directory, but you can customize that here.
  */
-defined( 'COMPOSER_PATH' ) || define( 'COMPOSER_PATH', ROOTPATH . 'vendor/autoload.php' );
+defined('COMPOSER_PATH') || define('COMPOSER_PATH', ROOTPATH . 'vendor/autoload.php');
 
 /*
  |--------------------------------------------------------------------------
@@ -33,14 +33,14 @@ defined( 'COMPOSER_PATH' ) || define( 'COMPOSER_PATH', ROOTPATH . 'vendor/autolo
  | Provide simple ways to work with the myriad of PHP functions that
  | require information to be in seconds.
  */
-defined( 'SECOND' ) || define( 'SECOND', 1 );
-defined( 'MINUTE' ) || define( 'MINUTE', 60 );
-defined( 'HOUR' ) || define( 'HOUR', 3600 );
-defined( 'DAY' ) || define( 'DAY', 86400 );
-defined( 'WEEK' ) || define( 'WEEK', 604800 );
-defined( 'MONTH' ) || define( 'MONTH', 2592000 );
-defined( 'YEAR' ) || define( 'YEAR', 31536000 );
-defined( 'DECADE' ) || define( 'DECADE', 315360000 );
+defined('SECOND') || define('SECOND', 1);
+defined('MINUTE') || define('MINUTE', 60);
+defined('HOUR')   || define('HOUR', 3600);
+defined('DAY')    || define('DAY', 86400);
+defined('WEEK')   || define('WEEK', 604800);
+defined('MONTH')  || define('MONTH', 2_592_000);
+defined('YEAR')   || define('YEAR', 31_536_000);
+defined('DECADE') || define('DECADE', 315_360_000);
 
 /*
  | --------------------------------------------------------------------------
@@ -67,16 +67,31 @@ defined( 'DECADE' ) || define( 'DECADE', 315360000 );
  |       http://tldp.org/LDP/abs/html/exitcodes.html
  |
  */
-defined( 'EXIT_SUCCESS' ) || define( 'EXIT_SUCCESS', 0 ); // no errors
-defined( 'EXIT_ERROR' ) || define( 'EXIT_ERROR', 1 ); // generic error
-defined( 'EXIT_CONFIG' ) || define( 'EXIT_CONFIG', 3 ); // configuration error
-defined( 'EXIT_UNKNOWN_FILE' ) || define( 'EXIT_UNKNOWN_FILE', 4 ); // file not found
-defined( 'EXIT_UNKNOWN_CLASS' ) || define( 'EXIT_UNKNOWN_CLASS', 5 ); // unknown class
-defined( 'EXIT_UNKNOWN_METHOD' ) || define( 'EXIT_UNKNOWN_METHOD', 6 ); // unknown class member
-defined( 'EXIT_USER_INPUT' ) || define( 'EXIT_USER_INPUT', 7 ); // invalid user input
-defined( 'EXIT_DATABASE' ) || define( 'EXIT_DATABASE', 8 ); // database error
-defined( 'EXIT__AUTO_MIN' ) || define( 'EXIT__AUTO_MIN', 9 ); // lowest automatically-assigned error code
-defined( 'EXIT__AUTO_MAX' ) || define( 'EXIT__AUTO_MAX', 125 ); // highest automatically-assigned error code
+defined('EXIT_SUCCESS')        || define('EXIT_SUCCESS', 0); // no errors
+defined('EXIT_ERROR')          || define('EXIT_ERROR', 1); // generic error
+defined('EXIT_CONFIG')         || define('EXIT_CONFIG', 3); // configuration error
+defined('EXIT_UNKNOWN_FILE')   || define('EXIT_UNKNOWN_FILE', 4); // file not found
+defined('EXIT_UNKNOWN_CLASS')  || define('EXIT_UNKNOWN_CLASS', 5); // unknown class
+defined('EXIT_UNKNOWN_METHOD') || define('EXIT_UNKNOWN_METHOD', 6); // unknown class member
+defined('EXIT_USER_INPUT')     || define('EXIT_USER_INPUT', 7); // invalid user input
+defined('EXIT_DATABASE')       || define('EXIT_DATABASE', 8); // database error
+defined('EXIT__AUTO_MIN')      || define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
+defined('EXIT__AUTO_MAX')      || define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+/**
+ * @deprecated Use \CodeIgniter\Events\Events::PRIORITY_LOW instead.
+ */
+define('EVENT_PRIORITY_LOW', 200);
+
+/**
+ * @deprecated Use \CodeIgniter\Events\Events::PRIORITY_NORMAL instead.
+ */
+define('EVENT_PRIORITY_NORMAL', 100);
+
+/**
+ * @deprecated Use \CodeIgniter\Events\Events::PRIORITY_HIGH instead.
+ */
+define('EVENT_PRIORITY_HIGH', 10);
 
 
 // include file kiểu này để mỗi lần update code thì check git nó tiện
