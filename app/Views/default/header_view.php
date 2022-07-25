a5089e86dd33711612d4c203f3040da87a2253ae
itvn9online <itvn9online@gmail.com>
2022-07-26 02:06:38 +0700

    sync

--- app/Views/default/header_view.php
+++ app/Views/default/header_view.php
@@ -7,43 +7,48 @@ if ( $isMobile == true ) {
 // desktop
 else {
     ?>
-<div class="top-bg">
-    <div class="w90 text-right bold cf">
-        <?php
+<section class="default-bg top-section medium">
+    <div class="row row-collapse">
+        <div class="col medium-12 small-12 large-12 text-right">
+            <div class="col-inner">
+                <?php
 
-        // nếu đã đăng nhập -> hiển thị menu profile
-        if ( $current_user_id > 0 ) {
-            // hiển thị thêm menu cho admin
-            if ( isset( $session_data[ 'userLevel' ] ) && $session_data[ 'userLevel' ] > 0 ) {
-                $menu_model->the_menu( 'top-admin-menu', 'top-login-menu' );
-            }
-            $menu_model->the_menu( 'top-profile-menu', 'top-login-menu' );
-        }
-        // chưa thì hiển thị menu đăng nhập/ đăng ký
-        else {
-            $menu_model->the_menu( 'top-login-menu' );
-        }
+                // nếu đã đăng nhập -> hiển thị menu profile
+                if ( $current_user_id > 0 ) {
+                    // hiển thị thêm menu cho admin
+                    if ( isset( $session_data[ 'userLevel' ] ) && $session_data[ 'userLevel' ] > 0 ) {
+                        $menu_model->the_menu( 'top-admin-menu', 'top-login-menu' );
+                    }
+                    $menu_model->the_menu( 'top-profile-menu', 'top-login-menu' );
+                }
+                // chưa thì hiển thị menu đăng nhập/ đăng ký
+                else {
+                    $menu_model->the_menu( 'top-login-menu' );
+                }
 
-        ?>
-    </div>
-</div>
-<div class="w96 top-position">
-    <div class="w90 cf top-menu d-flex">
-        <div class="lf f15">
-            <?php
-            $option_model->the_logo( $getconfig );
-            ?>
-        </div>
-        <div class="lf f75 top-nav-padding div-valign-center">
-            <?php
-            $menu_model->the_menu( 'top-nav-menu', 'main-nav-menu lf f85' );
-            ?>
-        </div>
-        <div class="lf f10 cf div-valign-center">
-            <button type="button" class="btn btn-primary btn-mobile-menu rf" data-bs-toggle="modal" data-bs-target="#searchModal" aria-label="Search"><i class="fa fa-search"></i> Tìm kiếm</button>
+                ?>
+            </div>
         </div>
     </div>
-</div>
+</section>
+<section class="">
+    <div class="row row-collapse align-middle">
+        <div class="col medium-3 small-3 large-3">
+            <div class="col-inner">
+                <?php
+                $option_model->the_logo( $getconfig );
+                ?>
+            </div>
+        </div>
+        <div class="col medium-9 small-9 large-9 text-right">
+            <div class="col-inner">
+                <?php
+                $menu_model->the_menu( 'top-nav-menu' );
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
 <?php
 } // END desktop
 
