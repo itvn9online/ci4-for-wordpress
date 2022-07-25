2b11010c1f842568a3fde1d56c9b3f8e31eea43a
itvn9online <itvn9online@gmail.com>
2022-07-25 22:25:24 +0700

    sync

---
+++ app/Config/CustomConstants.php
@@ -0,0 +1,186 @@
+<?php
+/*
+ * Constants mặc định của code gốc, file này mà xóa đi thì chỉ có ăn cám -> code này hết sử dụng luôn
+ */
+
+/////////////////////////////////////////////////// DAIDQ CONFIG ////////////////////////////////////////////////////////
+/*
+ * daidq: 2021-09-14
+ * Phần cấu hình thiết lập động để có thể tái sử dụng code cho nhiều website khác nhau mà không cần code lại nhiều
+ * Các controller, model... cố gắng viết theo quy tắc exten để có thể tái sử dụng
+ */
+
+defined( 'EBE_DATE_FORMAT' ) || define( 'EBE_DATE_FORMAT', 'Y-m-d' );
+defined( 'EBE_DATETIME_FORMAT' ) || define( 'EBE_DATETIME_FORMAT', 'Y-m-d H:i:s' );
+
+/*
+ * tạo đường dẫn admin tránh đường dẫn mặc định. Ví dụ : admin -> nhằm tăng cường bảo mật cho website
+ */
+defined( 'CUSTOM_ADMIN_URI' ) || define( 'CUSTOM_ADMIN_URI', 'wgr-wp-admin' );
+
+/*
+ * Các tham số khác, rất ít khi thay đổi
+ */
+define( 'PUBLIC_HTML_PATH', ROOTPATH );
+//echo PUBLIC_HTML_PATH . '<br>' . "\n";
+define( 'PUBLIC_PUBLIC_PATH', PUBLIC_HTML_PATH . 'public/' );
+//die( PUBLIC_PUBLIC_PATH );
+
+//
+define( 'VIEWS_PATH', APPPATH . 'Views/' );
+define( 'VIEWS_CUSTOM_PATH', VIEWS_PATH . 'custom/' );
+
+// Số lượng bản dịch dạng input -> website nào cần dùng nhiều tăng số lượng trong file functions lên
+defined( 'NUMBER_TRANS_INPUT' ) || define( 'NUMBER_TRANS_INPUT', 20 );
+// Số lượng bản dịch dạng textarea -> website nào cần dùng nhiều tăng số lượng trong file functions lên
+defined( 'NUMBER_TRANS_TEXTAREA' ) || define( 'NUMBER_TRANS_TEXTAREA', 10 );
+// khi cần thay label cho trang /admin/translates để dễ hiểu hơn thì thêm các thông số vào đây
+defined( 'TRANS_TRANS_LABEL' ) || define( 'TRANS_TRANS_LABEL', [
+    'custom_text0' => 'Bản dịch số 0',
+    'custom_textarea0' => 'Bản dịch số 0',
+] );
+
+
+/*
+ * lưu giá trị của config vào biến này, nếu hàm sau có gọi lại thì tái sử dụng luôn
+ */
+$this_cache_config = NULL;
+$this_cache_lang = NULL;
+
+
+/*
+ * Danh sách các custom taxonomy mà người dùng đăng ký sẽ được khai báo thêm ở đây
+ */
+$arr_custom_taxonomy = [];
+
+function register_taxonomy( $name, $ops = [] ) {
+    global $arr_custom_taxonomy;
+
+    $arr_custom_taxonomy[ $name ] = $ops;
+}
+
+
+/*
+ * Danh sách các custom post type mà người dùng đăng ký sẽ được khai báo thêm ở đây
+ */
+$arr_custom_post_type = [];
+
+function register_post_type( $name, $ops = [] ) {
+    global $arr_custom_post_type;
+
+    $arr_custom_post_type[ $name ] = $ops;
+}
+
+
+/*
+ * Thư mục chứa theme hiển thị cho website (tùy theo yêu cầu của khách hàng mà thiết lập giao diện khác nhau)
+ */
+// xác định theme tự động
+foreach ( glob( PUBLIC_PUBLIC_PATH . 'themes/*.actived-theme' ) as $filename ) {
+    $filename = basename( $filename, '.actived-theme' );
+    //echo $filename . '<br>' . "\n";
+    if ( is_dir( PUBLIC_PUBLIC_PATH . 'themes/' . $filename ) ) {
+        define( 'THEMENAME', $filename );
+        break;
+    }
+}
+
+// nếu không có file active theme tự động -> gán mặc định theme echbayfour
+defined( 'THEMENAME' ) || define( 'THEMENAME', 'echbayfour' );
+//echo THEMENAME . '<br>' . "\n";
+
+
+//
+define( 'THEMEPATH', PUBLIC_PUBLIC_PATH . 'themes/' . THEMENAME . '/' );
+//die( THEMEPATH );
+
+/*
+ * nạp file function của từng theme
+ */
+if ( file_exists( THEMEPATH . 'functions.php' ) ) {
+    include THEMEPATH . 'functions.php';
+}
+
+// website của nhà phát triển
+defined( 'PARTNER_WEBSITE' ) || define( 'PARTNER_WEBSITE', 'https://echbay.com/' );
+defined( 'PARTNER_BRAND_NAME' ) || define( 'PARTNER_BRAND_NAME', 'EchBay.com' );
+
+/*
+ * URL động cho website để có thể chạy trên nhiều tên miền khác nhau mà không cần config lại
+ */
+// tinh chỉnh protocol theo ý thích -> mặc định là https
+defined( 'BASE_PROTOCOL' ) || define( 'BASE_PROTOCOL', 'https' );
+// -> url động cho website
+define( 'DYNAMIC_BASE_URL', BASE_PROTOCOL . '://' . $_SERVER[ 'HTTP_HOST' ] . '/' );
+//die( DYNAMIC_BASE_URL );
+
+// khi cần chuyển các file tĩnh sang url khác để giảm tải cho server chính thì dùng chức năng này
+defined( 'CDN_BASE_URL' ) || define( 'CDN_BASE_URL', '' );
+
+// kiểu sử dụng cache
+defined( 'MY_CACHE_HANDLER' ) || define( 'MY_CACHE_HANDLER', 'file' );
+
+// chuỗi sẽ thêm vào khi sử dụng hàm mdnam -> md5
+defined( 'CUSTOM_MD5_HASH_CODE' ) || define( 'CUSTOM_MD5_HASH_CODE', $_SERVER[ 'HTTP_HOST' ] );
+
+// permission mặc định khi up file, tạo thư mục
+defined( 'DEFAULT_FILE_PERMISSION' ) || define( 'DEFAULT_FILE_PERMISSION', 0777 );
+defined( 'DEFAULT_DIR_PERMISSION' ) || define( 'DEFAULT_DIR_PERMISSION', 0777 );
+
+// với cache file -> thư mục lưu cache theo từng tên miền -> code thêm cho các web sử dụng domain pointer
+if ( MY_CACHE_HANDLER == 'file' ) {
+    define( 'WRITE_CACHE_PATH', WRITEPATH . 'cache/' . explode( ':', $_SERVER[ 'HTTP_HOST' ] )[ 0 ] . '/' );
+
+    //
+    define( 'CACHE_HOST_PREFIX', '' );
+}
+// với các thể loại cache khác -> sử dụng prefix -> do nó lưu vào ram thì không phân định theo path được
+else {
+    define( 'WRITE_CACHE_PATH', WRITEPATH . 'cache/' );
+
+    // tạo key theo host để làm prefix
+    define( 'CACHE_HOST_PREFIX', str_replace( '.', '', explode( ':', $_SERVER[ 'HTTP_HOST' ] )[ 0 ] ) );
+}
+//die( CACHE_HOST_PREFIX );
+//die( WRITE_CACHE_PATH );
+if ( !is_dir( WRITE_CACHE_PATH ) ) {
+    mkdir( WRITE_CACHE_PATH, DEFAULT_DIR_PERMISSION )or die( 'ERROR create cache dir' );
+    chmod( WRITE_CACHE_PATH, DEFAULT_DIR_PERMISSION );
+}
+
+// thời gian cache mặc định
+defined( 'MINI_CACHE_TIMEOUT' ) || define( 'MINI_CACHE_TIMEOUT', 300 );
+defined( 'MEDIUM_CACHE_TIMEOUT' ) || define( 'MEDIUM_CACHE_TIMEOUT', HOUR );
+defined( 'BIG_CACHE_TIMEOUT' ) || define( 'BIG_CACHE_TIMEOUT', 21600 ); // 6 giờ
+
+// cho phép sử dụng lệnh DELETE trong mysql
+defined( 'ALLOW_USING_MYSQL_DELETE' ) || define( 'ALLOW_USING_MYSQL_DELETE', false );
+
+/*
+ * Tiền tố cho danh mục sản phẩm
+ */
+defined( 'WGR_CATEGORY_PREFIX' ) || define( 'WGR_CATEGORY_PREFIX', 'category' );
+if ( WGR_CATEGORY_PREFIX != '' ) {
+    define( 'CATEGORY_BASE_URL', WGR_CATEGORY_PREFIX . '/' );
+} else {
+    define( 'CATEGORY_BASE_URL', '' );
+}
+
+/*
+ * Tiền tố cho trang tĩnh
+ */
+defined( 'WGR_PAGES_PREFIX' ) || define( 'WGR_PAGES_PREFIX', 'pages' );
+if ( WGR_PAGES_PREFIX != '' ) {
+    define( 'PAGE_BASE_URL', WGR_PAGES_PREFIX . '/' );
+} else {
+    define( 'PAGE_BASE_URL', '' );
+}
+
+/**
+ * Tiền tố cho bảng database.
+ * Chỉ sử dụng số, ký tự và dấu gạch dưới!
+ */
+defined( 'WGR_TABLE_PREFIX' ) || define( 'WGR_TABLE_PREFIX', 'wp_' );
+
+define( 'WGR_TERM_VIEW', WGR_TABLE_PREFIX . 'zzz_v_terms' );
+define( 'WGR_POST_VIEW', WGR_TABLE_PREFIX . 'zzz_v_posts' );
--- app/Config/MyRoutes.php
+++ app/Config/MyRoutes.php
@@ -1,65 +1,11 @@
 <?php
+/*
+ * Bạn có thể tự viết thêm Routes vào đây (có thể sẽ bị ghi đè bởi Routes gốc)
+ */
 
-// install
-$routes->get( '/install', 'Installs::index' );
+// Routes mặc định của bản code gốc, chỉ xóa bỏ khi thực sự cần thiết
+include __DIR__ . '/CustomRoutes.php';
 
-// admin
-// tạo đường dẫn admin tránh đường dẫn mặc định
-$routes->get( CUSTOM_ADMIN_URI, 'Admin\Dashboard::index' );
-
-// We get a performance increase by specifying the default
-// route since we don't have to scan directories.
-$routes->get( '/', 'Home::index' );
-
-//$routes->get( 'users', 'Users::index' );
-//$routes->get( 'guest', 'Guest::index' );
-
-//
-$routes->get( 'search', 'Search::index' );
-
-
-// sitemap
-$routes->get( 'sitemap', 'Sitemap::index' );
-$routes->get( 'sitemap.xml', 'Sitemap::index' );
-$routes->get( 'sitemap/(:segment)', 'Sitemap::index/$1' );
-$routes->get( 'sitemap/(:segment)/page/(:num)', 'Sitemap::index/$1/page/$2' );
-
-
-// blog
-$routes->get( 'blogs/(:segment)', 'Blogs::blogs_list/$1' );
-$routes->get( 'blogs/(:segment)/page/(:num)', 'Blogs::blogs_list/$1/page/$2' );
-$routes->get( 'blog-(:num)/(:segment)', 'Blogs::blog_details/$1/$2' );
-
-// post
-$routes->get( '(:num)/(:segment)', 'Posts::post_details/$1/$2' );
-
-// page -> có base slug cho page thì dùng loại base fix cứng này
-if ( WGR_PAGES_PREFIX != '' ) {
-    $routes->add( WGR_PAGES_PREFIX . '/(:segment)', 'Pages::get_page/$1' );
-}
-
-// custom post type
-$routes->match( [ 'get', 'post' ], 'p/(:segment)/(:num)/(:segment)', 'P::custom_post_type/$1/$2/$3' );
-
-// custom taxonomy
-$routes->get( 'c/(:segment)/(:num)/(:segment)', 'C::custom_taxonomy/$1/$2/$3' );
-$routes->get( 'c/(:segment)/(:num)/(:segment)/page/(:num)', 'C::custom_taxonomy/$1/$2/$3/page/$4' );
-
-// category -> có base slug cho category thì dùng loại base fix cứng này
-if ( WGR_CATEGORY_PREFIX != '' ) {
-    $routes->get( WGR_CATEGORY_PREFIX . '/(:segment)', 'Category::category_list/$1' );
-    $routes->get( WGR_CATEGORY_PREFIX . '/(:segment)/page/(:num)', 'Category::category_list/$1/page/$2' );
-}
-// không có thì mới sử dụng loại auto category -> hỗ trợ phân trang
-else {
-    $routes->get( '(:segment)/page/(:num)', 'Category::category_list/$1/page/$2' );
-}
-
-// auto category -> dùng nặng -> hạn chế sử dụng
-//$routes->get( '(:segment)/page/(:num)', 'Home::checkurl/$1/page/$2' );
-
-// hỗ trợ auto category và page -> sau đó sẽ so sánh URL, nếu khác biệt thì sẽ chuyển về link gốc
-$routes->add( '(:segment)', 'Home::checkurl/$1' );
-
-//
-//$routes->addPlaceholder( 'checkurl', '[0-9a-z]{1}-(:segment)' );
+/*
+ * Bạn có thể tự viết thêm Routes vào đây (ghi đè Routes gốc)
+ */
