60f8869393e038eb9a447d680ea95fe928cd6e7e
itvn9online <itvn9online@gmail.com>
2022-08-04 11:39:29 +0700

    sync

--- app/Views/private_view.php
+++ app/Views/private_view.php
@@ -14,14 +14,14 @@ $theme_private_view = str_replace( VIEWS_PATH, VIEWS_CUSTOM_PATH, $theme_default
 
 //
 if ( file_exists( $theme_private_view ) ) {
-    if ( $debug_enable === true )echo '<strong>' . str_replace( PUBLIC_HTML_PATH, '', $theme_private_view ) . '</strong><br>' . "\n";
+    if ( $debug_enable === true )echo '<div class="wgr-view-path bold">' . str_replace( PUBLIC_HTML_PATH, '', $theme_private_view ) . '</div>';
 
     //
     include $theme_private_view;
 }
 // không có thì nạp view mặc định
 else {
-    if ( $debug_enable === true )echo str_replace( PUBLIC_HTML_PATH, '', $theme_default_view ) . '<br>' . "\n";
+    //if ( $debug_enable === true )echo '<div class="wgr-view-path">' . str_replace( PUBLIC_HTML_PATH, '', $theme_default_view ) . '</div>';
 
     //
     include $theme_default_view;
