748687e04997a461aa0280ba6a4214f5b0fb38ee
itvn9online <itvn9online@gmail.com>
2022-08-04 11:16:36 +0700

    Update private_view.php

--- app/Views/private_view.php
+++ app/Views/private_view.php
@@ -10,13 +10,19 @@
 $theme_private_view = str_replace( VIEWS_PATH, VIEWS_CUSTOM_PATH, $theme_default_view );
 
 //
-if ( $debug_enable === true )echo str_replace( PUBLIC_HTML_PATH, '', $theme_default_view ) . '<br>' . "\n" . str_replace( PUBLIC_HTML_PATH, '', $theme_private_view ) . '<br>' . "\n";
+//if ( $debug_enable === true )echo str_replace( PUBLIC_HTML_PATH, '', $theme_default_view ) . '<br>' . "\n" . str_replace( PUBLIC_HTML_PATH, '', $theme_private_view ) . '<br>' . "\n";
 
 //
 if ( file_exists( $theme_private_view ) ) {
+    if ( $debug_enable === true )echo str_replace( PUBLIC_HTML_PATH, '', $theme_private_view ) . '<br>' . "\n";
+
+    //
     include $theme_private_view;
 }
 // không có thì nạp view mặc định
 else {
+    if ( $debug_enable === true )echo str_replace( PUBLIC_HTML_PATH, '', $theme_default_view ) . '<br>' . "\n";
+
+    //
     include $theme_default_view;
 }
