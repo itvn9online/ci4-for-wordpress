24f835449e25dc3ae1f35027b6722f5fc86c3afa
itvn9online <itvn9online@gmail.com>
2023-09-19 15:31:07 +0700

    custom 404 view

--- app/Views/404.php
+++ app/Views/404.php
@@ -1,13 +1,22 @@
 <?php
 
-$base_model->add_css( 'css/404.css', [
+//
+$base_model->adds_css([
+    'css/404.css',
+    'themes/' . THEMENAME . '/css/404.css',
+], [
     'cdn' => CDN_BASE_URL,
-] );
+]);
 
-?>
-<div class="text-center error-page">
-    <h1 data-h1="404">404</h1>
-    <p class="not-found" style="-webkit-text-stroke: #ccc 0.1px;" data-p="NOT FOUND">NOT FOUND</p>
-    <div class="msg_404 redcolor s14"><?php echo $msg_404; ?></div>
-</div>
-<div id="particles-js" class="text-center"> <a href="<?php echo DYNAMIC_BASE_URL; ?>">Về trang chủ</a> </div>
+// nạp view riêng của từng theme nếu có
+$theme_default_view = VIEWS_PATH . 'default/' . basename(__FILE__);
+//echo $theme_default_view;
+// nạp file kiểm tra private view
+include VIEWS_PATH . 'private_view.php';
+
+//
+$base_model->add_js('themes/' . THEMENAME . '/js/404.js', [
+    'cdn' => CDN_BASE_URL,
+], [
+    'defer'
+]);
---
+++ app/Views/default/404.php
@@ -0,0 +1,9 @@
+<br>
+<div class="text-center error-page">
+    <h1 data-h1="404">404</h1>
+    <p class="not-found" style="-webkit-text-stroke: #ccc 0.1px;" data-p="NOT FOUND">NOT FOUND</p>
+    <div class="msg_404 redcolor s14"><?php echo $msg_404; ?></div>
+</div>
+<div class="text-center"> <a href="<?php echo DYNAMIC_BASE_URL; ?>" class="btn btn-success">Về trang chủ</a> </div>
+<br>
+<br>
