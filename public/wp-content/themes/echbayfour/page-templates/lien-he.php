eacfc52f6cf44d5c5608e142630ef8c945c1e806
Dao Dai <itvn9online@gmail.com>
2025-03-08 11:36:07 +0700

    sync

--- public/wp-content/themes/echbayfour/page-templates/lien-he.php
+++ public/wp-content/themes/echbayfour/page-templates/lien-he.php
@@ -1,3 +1,17 @@
+<?php
+
+// 
+if (!empty($session_data)) {
+    $data_email = $session_data['user_email'];
+    $data_fullname = $session_data['display_name'];
+    $data_login = $session_data['user_login'];
+} else {
+    $data_email = '';
+    $data_fullname = '';
+    $data_login = '';
+}
+
+?>
 <div class="global-page-module w90-xoa">
     <div class="padding-global-content cf-xoa row">
         <div class="col col-main-content custom-width-global-main custom-width-page-main fullsize-if-mobile">
@@ -44,27 +58,28 @@
                         <?php $base_model->anti_spam_field(); ?>
                         <input type="hidden" name="to" value="comments" />
                         <input type="hidden" name="redirect" value="<?php echo $_SERVER['REQUEST_URI']; ?>" />
+                        <input type="hidden" name="data[login]" value="<?php echo $data_login; ?>" />
                         <div class="cf eb-contact-form">
                             <div class="lf f40 fullsize-if-mobile">
                                 <div class="form-group">
-                                    <label>Họ và tên:</label>
-                                    <input type="text" name="data[fullname]" class="form-control" value="" aria-required="true" required>
+                                    <label>Họ và tên <span class="redcolor">*</span></label>
+                                    <input type="text" name="data[fullname]" class="form-control" value="<?php echo $data_fullname; ?>" aria-required="true" required>
                                 </div>
                                 <br>
                                 <div class="form-group">
-                                    <label>Địa chỉ email:</label>
-                                    <input type="email" name="data[email]" class="form-control" value="" aria-required="true" required>
+                                    <label>Địa chỉ email <span class="redcolor">*</span></label>
+                                    <input type="email" name="data[email]" class="form-control" value="<?php echo $data_email; ?>" aria-required="true" required>
                                 </div>
                                 <br>
                                 <div class="form-group">
-                                    <label>Tiêu đề thông điệp:</label>
+                                    <label>Tiêu đề thông điệp <span class="redcolor">*</span></label>
                                     <input type="text" name="data[title]" class="form-control" value="" aria-required="true" required>
                                 </div>
                             </div>
                             <div class="lf f60 fullsize-if-mobile">
                                 <div class="left-menu-space40">
                                     <div class="form-group">
-                                        <label>Nội dung thông điệp:</label>
+                                        <label>Nội dung thông điệp <span class="redcolor">*</span></label>
                                         <textarea name="data[content]" class="form-control" rows="7" aria-required="true" required></textarea>
                                     </div>
                                     <br>
